ed903f80591ed2257673186e3f896a4c4402585f
Shubhang Dixit <shubhangd205@gmail.com>
2026-05-05 15:00:15 +0530

    initial commit

---
+++ src/main/java/com/example/collegeEventManagmenet/CollegeEventManagmenetApplication.java
@@ -0,0 +1,13 @@
+package com.example.collegeEventManagmenet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeEventManagmenetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeEventManagmenetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/collegeEventManagmenet/CollegeEventManagmenetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collegeEventManagmenet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeEventManagmenetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
